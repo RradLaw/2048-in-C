7826cec973e980e1875784322ac309bd6b523314
Jarrad Law <law0102@flinders.edu.au>
2014-03-27 20:31:37 +1030

    better looking output

--- test.c
+++ test.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include "2048.h"
 
-int line_vector_test(int i1,int i2,int i3,int i4,char *msg,
+int line_vector_test(char *s1, int i1,int i2,int i3,int i4,char *msg,
                 int o1,int o2,int o3,int o4, int (*func)(int,int *))
 {
   int list[4]={i1,i2,i3,i4};
   if (msg) printf("%s - ",msg); 
   else {
-    printf("Tilting {%d,%d,%d,%d} left yields {%d,%d,%d,%d} - ",
-             i1,i2,i3,i4,o1,o2,o3,o4);
+    printf("Tilting {%d,%d,%d,%d} %s yields {%d,%d,%d,%d} - ",
+             i1,i2,i3,i4,s1,o1,o2,o3,o4);
   }
   fflush(stdout);
   func(4,list);
   if ((list[0]!=o1)||(list[1]!=o2)||(list[2]!=o3)||(list[3]!=o4))
     {
-      printf("FAILED: {%d,%d,%d,%d} became {%d,%d,%d,%d} instead of"
+      printf("FAILED:\n {%d,%d,%d,%d} became {%d,%d,%d,%d} instead of"
              " {%d,%d,%d,%d}\n",
              i1,i2,i3,i4,list[0],list[1],list[2],list[3],
              o1,o2,o3,o4);
@@ -28,13 +28,13 @@ int line_vector_test(int i1,int i2,int i3,int i4,char *msg,
 int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
                int o1,int o2,int o3,int o4)
 {
-  return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_left);
+  return line_vector_test("left",i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_left);
 }
 
 int ttr_vector(int i1,int i2,int i3,int i4,char *msg,
                int o1,int o2,int o3,int o4)
 {
-  return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_right);
+  return line_vector_test("right",i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_right);
 }
 
 int test_tilt_left()
