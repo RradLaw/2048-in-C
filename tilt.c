b96f3cf60392d342db8935e9b9fe0123c5d06811
Jarrad Law <law0102@flinders.edu.au>
2014-03-27 20:32:59 +1030

    updated right tilt function

--- tilt.c
+++ tilt.c
@@ -35,15 +35,27 @@ int tilt_line_right(int length,int *line){
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
-  int temp[length];
-  int i;
-  
-  for(i=0;i<length;i++){
-    temp[length-i]=line[i];
-  }
-  tilt_line_left(length,temp);
-  for(i=0;i<length;i++){
-    line[i]=temp[i];
-  }
+  // slide tiles to the left
+  int i,j,shift;
+  for (i=length;i>=0;i--){
+    shift=0;
+    if(!line[i]){
+      for(j=i-1;j>=0;j--){
+        if(line[j]){
+          line[i]=line[j];
+          line[j]=0;
+          j=0;
+          shift++;
+        }
+      }
+    }
+  // combine tiles as required
+    if(shift) i++;
+    if(line[i]==line[i-1]){
+      line[i]=2*line[i];
+      line[i-1]=0;
+    }
+    if(shift) i--;
+  } 
   return 0;
 }
