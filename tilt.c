e54607599215891a9aa5f880a6ea9c1fcfa12f1c
Jarrad Law <law0102@flinders.edu.au>
2014-03-27 15:42:45 +1030

    Initial tile_line_left code

--- tilt.c
+++ tilt.c
@@ -5,9 +5,20 @@ int tilt_line_left(int length,int *line)
 {
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
-
+  int i;
+  for (i=length-1;i>1;i--){
+  	if(line[i-1]=0){
+  		line[i-1]=line[i];
+  		line[i]=0;
+  		i--;
+  	}
+  	else if(line[i-1]=line[i]){
+  		line[i-1]*=2;
+  		line[i]=0;
+  		i--;
+  	}
+  }
   // slide tiles to the left
-
 
   // combine tiles as required
 
