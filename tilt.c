6fe0b02fd5a79e4ab34415e5be99dbb7bec150c0
Jarrad Law <law0102@flinders.edu.au>
2014-03-27 17:15:53 +1030

    added functional left shifting

--- tilt.c
+++ tilt.c
@@ -5,19 +5,20 @@ int tilt_line_left(int length,int *line)
 {
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
-  int i;
-  for (i=length-1;i>1;i--){
-  	if(line[i-1]=0){
-  		line[i-1]=line[i];
-  		line[i]=0;
-  		i--;
-  	}
-  	else if(line[i-1]=line[i]){
-  		line[i-1]*=2;
-  		line[i]=0;
-  		i--;
+  
+  int i,j;
+  for (i=0;i<length;i++){
+  	if(!line[i]){
+      for(j=i+1;j<length;j++){
+        if(line[j]){
+          line[i]=line[j];
+          line[j]=0;
+          j=length;
+        }
+      }
   	}
   }
+  
   // slide tiles to the left
 
   // combine tiles as required
