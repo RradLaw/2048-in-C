940420a7fe8697dcfc48705e2a6611561b77d63c
Jarrad Law <law0102@flinders.edu.au>
2014-03-29 21:05:08 +1030

    added up and down tilting

--- main.c
+++ main.c
@@ -11,24 +11,31 @@ int main(int argc,char **argv)
   srandom(time(0));
   
   int board_size=4;
+  int i=0;
   int **board=board_create(board_size);
   if (!board) {
     printf("failed to create board.\n");
     exit(0);
   }
-
+board_spawn_tile(board_size,board);
+board_spawn_tile(board_size,board);
   while(1) {
     board_display(board_size,board);
     printf("Next move? "); fflush(stdout);
     int action=read_input();
     switch(action) {
     case GO_LEFT:
+      int *line
       break;
     case GO_RIGHT:
       break;
     case GO_UP:
+      for(i=0;i<board_size;i++) tilt_line_left(board_size,board[i]);
+      board_spawn_tile(board_size,board);
       break;
     case GO_DOWN:
+      for(i=0;i<board_size;i++) tilt_line_right(board_size,board[i]);
+      board_spawn_tile(board_size,board);
       break;
     case GO_NOWHERE:
       break;
