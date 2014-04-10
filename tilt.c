1171298566b71e83111d0986ec7776e40109deb4
law0102 <law0102@flinders.edu.au>
2014-04-10 15:36:40 +0930

    corrected tilted returns

--- tilt.c
+++ tilt.c
@@ -8,6 +8,7 @@ int tilt_line_left(int length,int *line)
 
   // slide tiles to the left
   int i,j,shift;
+  int tilted=0;
   int merged=0;
   for (i=0;i<length;i++){
     shift=0;
@@ -18,19 +19,21 @@ int tilt_line_left(int length,int *line)
           line[j]=0;
           j=length;
           shift++;
+          tilted=1;
         }
       }
   	}
   // combine tiles as required
     if(shift) i--;
-    if(line[i]==line[i+1]&&!merged){
+    if(line[i]==line[i+1]&&!merged&&line[i]){
       line[i]=2*line[i];
       line[i+1]=0;
       merged=1;
+      tilted=1;
     }else merged=0;
     if(shift) i++;
   } 
-  return 0;
+  return tilted;//not working
 }
 
 int tilt_line_right(int length,int *line){
@@ -39,6 +42,7 @@ int tilt_line_right(int length,int *line){
 
   // slide tiles to the right
   int i,j,shift;
+  int tilted=0;
   int merged=0;
   for (i=length-1;i>=0;i--){
     shift=0;
@@ -49,17 +53,19 @@ int tilt_line_right(int length,int *line){
           line[j]=0;
           j=0;
           shift++;
+          tilted=1;
         }
       }
     }
   // combine tiles as required
     if(shift) i++;
-    if(line[i]==line[i-1]&&!merged){
+    if(line[i]==line[i-1]&&!merged&&line[i]){
       line[i]=2*line[i];
       line[i-1]=0;
       merged=1;
+      tilted=1;
     } else merged=0;
     if(shift) i--;
   } 
-  return 0;
+  return tilted;
 }
